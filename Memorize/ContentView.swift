7fe6fe0b79dc99d353acde4f5fe3796e54f5e3cd
Samuel Machado <samuelmachado1@gmail.com>
2021-08-17 16:32:39 -0300

    ft(develop): Creating project and initial structure

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Samuel Estrella on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView : View {
+    var isFaceUp: Bool
+    var body: some View{
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            }else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+            
+        }
+    }
+    
+    
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Samuel Estrella on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
